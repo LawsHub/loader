aa67f789d13015907cd6bea12510d3fc6835be9f
¿zey <adiciurila1999@gmail.com>
2025-11-16 16:20:19 +0200

    Fix formatting in loader.lua for URL entries

--- loader.lua
+++ loader.lua
@@ -83,8 +83,8 @@ local scripts = {
 	[96342491571673] = "https://raw.githubusercontent.com/LawsHub/loader/refs/heads/main/Sab",
 
 	-- Dead Rails
-	[116495829188952] = "https://pastebin.com/raw/WQXFaTeF"
-	[70876832253163] = "https://raw.githubusercontent.com/LawsHub/loader/refs/heads/main/Dead%20Rails"
+	[116495829188952] = "https://pastebin.com/raw/WQXFaTeF",
+	[70876832253163] = "https://raw.githubusercontent.com/LawsHub/loader/refs/heads/main/Dead%20Rails",
 }
 
 local url = scripts[game.PlaceId]
